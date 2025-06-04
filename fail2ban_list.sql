184a4dcf7ac365ae451fbb0febd3bce5cb8d8892
John Studnicka <contact@zentec.dev>
2025-06-04 03:46:01 -0700

    Create fail2ban_list.sql

---
+++ fail2ban_list.sql
@@ -0,0 +1,10 @@
+.headers on
+.mode column
+SELECT
+    jail,
+    ip,
+    CASE
+        WHEN bantime = -1 THEN 'Permanent'
+        ELSE (timeofban + bantime) - strftime('%s', 'now')
+    END AS remaining_time
+FROM bans;
