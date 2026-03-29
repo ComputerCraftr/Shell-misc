f4ca719a704b7475e13ab008de2d3e9fcb1d6c79
John Studnicka <contact@zentec.dev>
2026-03-28 22:17:49 -0700

    Improve shell script logging

--- termux/boot/start-services.sh
+++ termux/boot/start-services.sh
@@ -1,6 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 PREFIX="${PREFIX:-/data/data/com.termux/files/usr}"
 
+printf '%s\n' "[termux:boot] acquiring wake lock and starting services" >&2
 termux-wake-lock
-# shellcheck source=/dev/null
+# shellcheck source=/data/data/com.termux/files/usr/etc/profile.d/start-services.sh
+# shellcheck disable=SC1091
 source "${PREFIX}/etc/profile.d/start-services.sh"
